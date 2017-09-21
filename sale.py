8fff41748e01f4d1886b4ec25c427138167f57e0
?ngel ?lvarez <angel@nan-tic.com>
2017-09-21 12:15:04 +0200

    create and confirm milestons if milestone_type defined

--- sale.py
+++ sale.py
@@ -1,7 +1,7 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.model import fields
-from trytond.pool import PoolMeta
+from trytond.pool import PoolMeta, Pool
 from trytond.pyson import Eval
 
 __all__ = ['Sale']
@@ -19,5 +19,23 @@ class Sale:
 
     def _get_project(self):
         project = super(Sale, self)._get_project()
-        project.milestone_group_type = self.milestone_type
+        if project:
+            project.milestone_group_type = self.milestone_type
         return project
+
+    @classmethod
+    def create_projects(cls, sales):
+        super(Sale, cls).create_projects(sales)
+        pool = Pool()
+        Work = pool.get('project.work')
+        Milestone = pool.get('project.invoice_milestone')
+        projects = set()
+        for sale in sales:
+            for line in sale.lines:
+                if line.project:
+                    projects.add(line.project)
+        Work.create_milestone(projects)
+        milestones = []
+        for p in projects:
+            milestones += p.milestones
+        Milestone.confirm(milestones)
