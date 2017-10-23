325c38198e2d85323dc5b5b0319b5c3fbb65a05f
Raimon Esteve <raimon@nan-tic.com>
2017-10-23 17:01:01 +0200

    Reset bank account when sale payment type has not bank account task-032452

--- milestone.py
+++ milestone.py
@@ -18,5 +18,9 @@ class Milestone:
                 invoice.payment_term = sale.payment_term
             if hasattr(sale, 'payment_type') and sale.payment_type:
                 invoice.payment_type = sale.payment_type
+                if hasattr(invoice, 'bank_account'):
+                    # reset bank account in case sale payment type is not
+                    # same default invoice payment type and not has bank account
+                    invoice.on_change_with_bank_account()
             break
         return invoice
